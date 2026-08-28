957a262a92d029eb06a1af426662b36c0a5b70fa
Mount Pisgah SDA <MtPisgah2026.mtpisgahsda@gmail.com>
2026-08-28 02:44:59 -0400

    added exception errors & logs

--- src/launcher.py
+++ src/launcher.py
@@ -3,8 +3,8 @@ from pathlib import Path
 import json
 
 # this function points to the config file and loads it into a dictionary
-# it turns into a dictionary with this line: return json.load(file)
 # the .open() method opens the file in read mode and the encoding is set to utf-8
+# it turns into a dictionary with this line: return json.load(file)
 def load_config():
     """
     Load configuration settings from a config file.
@@ -16,8 +16,17 @@ def load_config():
 
 # this function launches an application given its path
 # it uses the subprocess library to run the application in a new process
-def launch_app(app_path: str):
+def launch_app(app_name: str, app_path: str):
     """
-    Launch an application given its path.
+    attempt to launch an application.
+    Returns True when successful and False when unsuccessful.
     """
-    subprocess.Popen([app_path])
+    try:
+        subprocess.Popen([app_path])
+        return True
+
+    except FileNotFoundError:
+        return False
+
+    except Exception as error:
+        return False
--- src/main.py
+++ src/main.py
@@ -1,10 +1,23 @@
+import logging
+from pathlib import Path
 from launcher import load_config, launch_app
+
+LOG_DIR = Path(__file__).resolve().parent.parent / "logs"
+LOG_DIR.mkdir(exist_ok=True)
+
+LOG_FILE = LOG_DIR / "broadcast_launcher.log"
+
+logging.basicConfig(
+    filename=LOG_FILE,
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(message)s"
+)
 
 #load_config() returns a dictionary. config["apps"] gives me a list. 
 #The for loop takes one dictionary from that list at a time, 
 #and I use its keys to get the name and path.
 def main():
-    print("Broadcast Launcher Starting...")
+    logging.info("Broadcast Launcher Starting...")
 
     # step 1: read apps.json file and load it into a dictionary
     config = load_config()
@@ -17,11 +30,16 @@ def main():
         #     "name": "Chrome",
         #     "path": "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
         # }
-        print(f"Launching {app['name']} from {app['path']}...")
+        logging.info("Attempting to launch %s", app['name'])
 
         #step #3 takes the path from the loop 
         # and passes it to the launch_app function which uses the subprocess library to run the application
-        launch_app(app['path'])
+        success = launch_app(app['name'], app['path'])
+
+        if success:
+            logging.info("%s lauched successfuly", app['name'])
+        else:
+            logging.error("%s failed to lauch", app['name'])
     
 
 if __name__ == "__main__":
